0c788e3302135f640ee53a3b293aa7405093a28c
kyorohiro <kyorohiro@gmail.com>
2018-10-09 04:07:38 +0000

    7.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   get '/signup', to: 'users#new'
+
   get 'static_pages/home' => "static_pages/home"
   get 'static_pages/help'
   get 'static_pages/about'
