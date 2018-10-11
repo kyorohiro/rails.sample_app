8b7a1c21bcdaee2954060a6732d16d4b0ead83db
kyorohiro <kyorohiro@gmail.com>
2018-10-11 14:18:27 +0000

    7.4

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+ test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "kyorohiro",
+                                         email: "kyorohiro@gmail.com",
+                                         password:              "miraigan",
+                                         password_confirmation: "miraigan" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
